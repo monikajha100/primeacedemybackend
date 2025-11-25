ec1795cd53e313899555cba7a228f4181e65650e
monikajha100 <monikailasharma@gmail.com>
2025-11-25 21:33:59 +0530

    Fix JWT utility type errors

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -12,7 +12,7 @@ export interface JWTPayload {
 
 export const generateToken = (payload: JWTPayload): string => {
   const options: SignOptions = {
-    expiresIn: JWT_EXPIRES_IN as string | number,
+  expiresIn: JWT_EXPIRES_IN as any,
   };
   return jwt.sign(payload, JWT_SECRET, options);
 };
